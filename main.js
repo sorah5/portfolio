78b6b8d39a8b8e653a813dc5710fcc4102a7be03
sorah5 <74638321+sorah5@users.noreply.github.com>
2021-03-17 11:08:31 +0900

    js_navbar_menu_click

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ document.addEventListener('scroll',() =>{
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// 네비바 메뉴 누를떄 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+});
